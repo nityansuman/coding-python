5e6db7b0e29e44b373c3db0cbc014679dfc9a807
nityansuman <nityan.suman@gmail.com>
2021-06-25 21:37:24 +0530

    Updated

--- text_alignment.py
+++ text_alignment.py
@@ -21,7 +21,7 @@ This method returns a right aligned string of length width.
 
 Print the symbol of hackerrank using a set symbol in 2D
 
-    H
+	H
    HHH
   HHHHH
  HHHHHHH
@@ -41,37 +41,37 @@ HHHHHHHHH
   HHHHH               HHHHH
   HHHHH               HHHHH
   HHHHH               HHHHH
-                    HHHHHHHHH
-                     HHHHHHH
-                      HHHHH
-                       HHH
-                        H
+					HHHHHHHHH
+					 HHHHHHH
+					  HHHHH
+					   HHH
+						H
 """
 
 if __name__ == "__main__":
-    # Thickness must be an odd number
-    THISKNESS = int(input())
-    S = "H"
+	# Thickness must be an odd number
+	THISKNESS = int(input())
+	S = "H"
 
-    # Top cone
-    for i in range(THISKNESS):
-        print((S * i).rjust(THISKNESS - 1) + S + (S * i).ljust(THISKNESS - 1))
+	# Top cone
+	for i in range(THISKNESS):
+		print((S * i).rjust(THISKNESS - 1) + S + (S * i).ljust(THISKNESS - 1))
 
-    # Top pillars
-    for i in range(THISKNESS + 1):
-        print((S * THISKNESS).center(THISKNESS * 2) +
-              (S * THISKNESS).center(THISKNESS * 6))
+	# Top pillars
+	for i in range(THISKNESS + 1):
+		print((S * THISKNESS).center(THISKNESS * 2) +
+			  (S * THISKNESS).center(THISKNESS * 6))
 
-    # Middle belt
-    for i in range((THISKNESS + 1) // 2):
-        print((S * THISKNESS * 5).center(THISKNESS * 6))
+	# Middle belt
+	for i in range((THISKNESS + 1) // 2):
+		print((S * THISKNESS * 5).center(THISKNESS * 6))
 
-    # Bottom pillars
-    for i in range(THISKNESS + 1):
-        print((S * THISKNESS).center(THISKNESS * 2) +
-              (S * THISKNESS).center(THISKNESS * 6))
+	# Bottom pillars
+	for i in range(THISKNESS + 1):
+		print((S * THISKNESS).center(THISKNESS * 2) +
+			  (S * THISKNESS).center(THISKNESS * 6))
 
-    # Bottom cone
-    for i in range(THISKNESS):
-        print(((S * (THISKNESS - i - 1)).rjust(THISKNESS) + S
-               + (S * (THISKNESS - i - 1)).ljust(THISKNESS)).rjust(THISKNESS * 6))
+	# Bottom cone
+	for i in range(THISKNESS):
+		print(((S * (THISKNESS - i - 1)).rjust(THISKNESS) + S
+			   + (S * (THISKNESS - i - 1)).ljust(THISKNESS)).rjust(THISKNESS * 6))
