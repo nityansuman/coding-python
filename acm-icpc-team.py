a3e04ed250190d451f04d2a22dac33dd77f81d7e
nityansuman <nityan.suman@gmail.com>
2021-06-28 03:08:52 +0530

    Adding acm-icpc team problem

---
+++ acm-icpc-team.py
@@ -0,0 +1,50 @@
+# Import packages
+import os
+
+
+def acmTeam(topic):
+	# Convert string to binary
+	topic = [int(t, 2) for t in topic]
+
+	# Counter
+	max_topics, num_teams = 0, 0
+
+	# Iterate over topic
+	for i in range(len(topic) - 1):
+		for j in range(i + 1, len(topic)):
+			# Count learned topics
+			topics = bin(topic[i] | topic[j]).count("1")
+			if(topics == max_topics):
+				num_teams += 1
+			if(topics > max_topics):
+				max_topics = topics
+				num_teams = 1
+	return [max_topics, num_teams]
+
+
+if __name__ == "__main__":
+	# Read input from file
+	fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+	# Process input
+	first_multiple_input = input().rstrip().split()
+	n = int(first_multiple_input[0])
+	m = int(first_multiple_input[1])
+
+	# Collection placeholder
+	topic = list()
+
+	# Read input from stdin
+	for _ in range(n):
+		topic_item = input()
+		topic.append(topic_item)
+
+	# Call method to get results
+	result = acmTeam(topic)
+
+	# Write to file
+	fptr.write('\n'.join(map(str, result)))
+	fptr.write('\n')
+
+	# Close file
+	fptr.close()
