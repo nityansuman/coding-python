e5a98f6cdb3496b9890e51bbdb644232c1927996
nityansuman <nityan.suman@gmail.com>
2021-07-04 06:05:04 +0530

    Adding largest inorder span solution

---
+++ interview/largest_inorder_span.py
@@ -0,0 +1,63 @@
+"""
+INTERVIEW QUESTION
+
+Given a seqeunce of characters (string) (all same case), find the largest in-order span.
+
+An in-order span is defined as set of characters in ASCII increasing order
+with or without gaps i.e., `abf` is in-order but `fb` is not in-order.
+
+Note:
+- In case of multiple string spans with same length print the first one.
+- Input sequence can have repeatition of characters.
+
+Ex:
+	abcde -> abcde
+	deabc -> abc
+	cdeah -> cde
+	ababc -> abc
+"""
+
+def largest_inorder_span(str):
+	# Set variables
+	prev_char = None
+	span_index, global_span_index = list(), list()
+
+	for c in str:
+		# Get character index
+		index = str.index(c)
+
+		if prev_char is None:
+			# Set first character index
+			span_index.append(index)
+		else:
+			if c > prev_char:
+				# Add character index
+				span_index.append(index)
+			else:
+				if len(span_index) > len(global_span_index):
+					# Update global span index
+					global_span_index = span_index
+					# Reset span index
+					span_index = [index]
+
+		# Set current character to previous for lookup
+		prev_char = c
+
+	# Update global span index for edge caase handling
+	if len(span_index) > len(global_span_index):
+		global_span_index = span_index
+
+	# Construct in-order string
+	return "".join([str[i] for i in global_span_index])
+
+
+if __name__ == "__main__":
+	# Read integer input denoting number of tests cases from stdin
+	tests = int(input().strip())
+
+	for t in range(tests):
+		# Read string input from stdin
+		str = input().strip()
+
+		# Get largest in-order span
+		print(largest_inorder_span(str))
