97e6b83df876c474a316dfe2ba0fc4c4f9508afe
nityansuman <nityan.suman@gmail.com>
2021-07-13 19:43:57 +0530

    Adding binary search algorithm implementation

---
+++ algorithms/binary_search.py
@@ -0,0 +1,43 @@
+
+def binary_search(arr, search):
+	# Check for empty array
+	if len(arr) == 0:
+		raise AssertionError("Empty array found!")
+
+	# Check for single element in array
+	if len(arr) == 1:
+		if arr[0] == search:
+			return True
+		else:
+			return False
+
+	# Compute middle index
+	if len(arr) % 2 == 0:
+		mid = len(arr) // 2
+	else:
+		mid = len(arr) // 2
+
+	# Check in middle index
+	if arr[mid] == search:
+		return True
+	else:
+		# Recursive call to either side of the array
+		if search <= arr[mid]:
+			return binary_search(arr[0:mid], search)
+		else:
+			return binary_search(arr[mid:], search)
+
+
+# Test
+if __name__ == "__main__":
+	# Read sorted array from stdin
+	arr = list(map(int, input().strip().split()))
+
+	# Read search array from stdin
+	search = list(map(int, input().strip().split()))
+
+	# Search in the array
+	for s in search:
+		print(f"Searching for {s} in [{arr}]:", end=" -> ")
+		flag = binary_search(arr, s)
+		print("Found:", flag)
