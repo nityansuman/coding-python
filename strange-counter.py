0b1ec7b3693d1e5c788e32eedcfeacbb693fccaa
nityansuman <nityan.suman@gmail.com>
2021-08-04 04:19:32 +0530

    Restructured code

--- strange-counter.py
+++ strange-counter.py
@@ -1,18 +1,16 @@
-
 if __name__ == "__main__":
-	# Read input from stdin
-	T = int(input().strip())
+    # Read number of test cases input from stdin
+    T = int(input().strip())
 
-	# Set counter start
-	X = 3
+    X = 3 # Counter start
 
-	# Placeholder variables
-	total, diff, n = 0, 0, 0
+    # Placeholder variables
+    total, diff, n = 0, 0, 0
 
-	# Compute final counter value
-	while T > total:
-		diff = (X * 2**(n+1)) - (X * 2**n)
-		total += diff
-		n += 1
+    # Compute counter
+    while T > total:
+        diff = (X * 2**(n+1)) - (X * 2**n)
+        total += diff
+        n += 1
 
-	print(total - T + 1)
+    print(total - T + 1)
