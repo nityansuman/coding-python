ae777c1e4c0af2727de525a176d7e33fabf367bb
nityansuman <nityan.suman@gmail.com>
2021-08-04 04:50:20 +0530

    Restructured code

--- angry-professor.py
+++ angry-professor.py
@@ -1,39 +1,18 @@
-# Import packages
-import os
-
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. INTEGER k
-#  2. INTEGER_ARRAY a
-#
-
-def angryProfessor(k, a):
-	a = sorted(a, key=int)
-	return "NO" if a[k-1] <= 0 else "YES"
+def angry_professor(k: int, arr: list) -> str:
+    return "NO" if arr[k-1] <= 0 else "YES"
 
 
 if __name__ == "__main__":
-	# Open file to write output
-	fptr = open(os.environ["OUTPUT_PATH"], "w")
+    # Read number test cases
+    num_test = int(input().strip())
 
-	# Read test cases input from stdin
-	t = int(input().strip())
+    for t in range(num_test):
+        # Read students in class (n) and cancelation threshold (k)
+        n, k = map(int, input().strip().split())
 
-	for t_itr in range(t):
-		# Read input from stdin
-		first_multiple_input = input().rstrip().split()
+        # Read n-separated integers describing student arrival times
+        arr = list(map(int, input().rstrip().split()))
+        arr = sorted(arr, key=int)
 
-		# Process read input
-		n = int(first_multiple_input[0])
-		k = int(first_multiple_input[1])
-		a = list(map(int, input().rstrip().split()))
-
-		# Execute method to get results
-		result = angryProfessor(k, a)
-
-		# Write output to file
-		fptr.write(result + "\n")
-
-	# Close file
-	fptr.close()
+        # Determine if class is canceled
+        print("Is class canceled:", angry_professor(k, arr))
