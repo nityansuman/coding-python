fcc233a0650375d399086eee2a717ef4a3835f78
nityansuman <nityan.suman@gmail.com>
2021-08-04 04:54:05 +0530

    Restructured code

--- beautiful-days.py
+++ beautiful-days.py
@@ -1,16 +1,13 @@
-
 if __name__ == "__main__":
-	# Read input from stdin
-	N, M, K = [int(t) for t in input().strip().split(" ")]
+    # Read input from stdin
+    n, m, k = list(map(int, input().strip().split()))
 
-	# Set counter
-	count = 0
+    # Count beautiful days
+    count = 0
+    for x in range(n, m+1):
+        y = int(str(x)[::-1])
 
-	# Count beautiful days
-	for x in range(N, M+1):
-		y = int(str(x)[::-1])
+        if (abs(x - y) % k) == 0:
+            count += 1
 
-		if (abs(x - y) % K) == 0:
-			count += 1
-
-	print(count)
+    print("Number of beatiful days:", count)
