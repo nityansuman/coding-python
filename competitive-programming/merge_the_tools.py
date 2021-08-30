d3180845874505502d3518225872ac79e95c843f
nityansuman <nityan.suman@gmail.com>
2021-08-31 03:00:49 +0530

    Added comments

--- competitive-programming/merge_the_tools.py
+++ competitive-programming/merge_the_tools.py
@@ -5,13 +5,17 @@ def create_sequences(sequence, factor):
 
 	# Iteratively create new sequences
 	for i in range(0, seq_len, factor):
+		# Unique character set placeholder
 		seen = set()
 		for char in sequence[i:i+factor]:
 			if char not in seen:
+				# If not seen, print
 				seen.add(char)
 				print(char, end="")
 			else:
+				# If seen, ignore
 				continue
+		# Add newline for next sequence
 		print()
 
 
