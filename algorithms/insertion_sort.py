02b9ac20612fa2e35d295105f81fa42f8bf7f304
nityansuman <nityan.suman@gmail.com>
2021-09-01 05:18:49 +0530

    Updated syntax

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,44 +1,40 @@
-"""Insertion sort implementation.
-"""
+# Insertion Sort Implementation
 
 def insertion_sort(arr, ascending=True):
-	# Raise error if array is empty
-	if len(arr) == 0:
-		raise AssertionError("Empty array found.")
+    # Raise error if array is empty
+    if len(arr) == 0:
+        raise AssertionError("Empty array found.")
 
-	# Return array if only one element found
-	if len(arr) == 1:
-		return arr
+    # Return array if only one element found
+    if len(arr) == 1:
+        return arr
 
-	# Iterate over the array
-	for i in range(1, len(arr)):
-		# Set first key at index 1
-		key = arr[i]
+    # Iterate over the array
+    for i in range(1, len(arr)):
+        # Set first key at index 1
+        key = arr[i]
 
-		# Iterate backwards and create space
-		j = i-1
+        # Iterate backwards and create space
+        j = i-1
 
-		if ascending:
-			while j >= 0 and key < arr[j]:
-				arr[j+1] = arr[j]
-				j -= 1
-		else:
-			while j >= 0 and key > arr[j]:
-				arr[j+1] = arr[j]
-				j -= 1
-		# Add key at appropriate place
-		arr[j+1] = key
+        if ascending:
+            while j >= 0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+        else:
+            while j >= 0 and key > arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+        # Add key at appropriate place
+        arr[j+1] = key
 
-	return arr
+    return arr
 
 
-# Test
 if __name__ == "__main__":
-	# Read input array from stdin
-	arr = list(map(int, input().strip().split()))
+    # Read input array from stdin
+    arr = list(map(int, input().strip().split()))
 
-	# Compute method to perform insertion sort
-	sorted_arr = insertion_sort(arr, ascending=True)
-
-	# View sorted array
-	print("Sorted array:", sorted_arr)
+    # Perform insertion sort
+    sorted_arr = insertion_sort(arr, ascending=True)
+    print("Sorted array:", sorted_arr)
