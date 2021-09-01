4fe3377b57b71e09cf6aa2c6a1d4af004ef5f0be
nityansuman <nityan.suman@gmail.com>
2021-09-02 04:12:45 +0530

    Adding solution to column based sorting problem

---
+++ competitive-programming/athlete_sort.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+	# Read number of rows and columns from stdin
+	rows, columns = list(map(int, input().strip().split()))
+
+	# Read data points from stdin
+	data = list()
+	for i in range(rows):
+		data.append(list(map(int, input().strip().split())))
+
+	# Read sprting index from stdin
+	sort_col_index = int(input().strip())
+
+	# Sort data using a particular column index
+	for row in sorted(data, key=lambda t: t[sort_col_index]):
+		print(" ".join([str(x) for x in row]).strip())
