8bc0d95ed4dae5272f979080a0cba400e5773b48
nityansuman <nityan.suman@gmail.com>
2021-09-06 04:10:32 +0530

    Updated

--- competitive-programming/angry_professor.py
+++ competitive-programming/angry_professor.py
@@ -1,18 +1,18 @@
 def angry_professor(k: int, arr: list) -> str:
-    return "NO" if arr[k-1] <= 0 else "YES"
+	return "NO" if arr[k-1] <= 0 else "YES"
 
 
 if __name__ == "__main__":
-    # Read number test cases
-    num_test = int(input().strip())
+	# Read number test cases
+	num_test = int(input().strip())
 
-    for t in range(num_test):
-        # Read students in class (n) and cancelation threshold (k)
-        n, k = map(int, input().strip().split())
+	for t in range(num_test):
+		# Read students in class (n) and cancelation threshold (k)
+		n, k = map(int, input().strip().split())
 
-        # Read n-separated integers describing student arrival times
-        arr = list(map(int, input().rstrip().split()))
-        arr = sorted(arr, key=int)
+		# Read n-separated integers describing student arrival times
+		arr = list(map(int, input().rstrip().split()))
+		arr = sorted(arr, key=int)
 
-        # Determine if class is canceled
-        print("Is class canceled:", angry_professor(k, arr))
+		# Determine if class is canceled
+		print("Is class canceled:", angry_professor(k, arr))
