472e737e2dcfa38828bdce984ca42ccfdb883bcc
Kumar Nityan Suman <nityan.suman@gmail.com>
2022-11-19 17:43:20 +0000

    Adding solution

---
+++ leetcode/9_palindrome_number.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/palindrome-number/
+
+
+class Solution:
+	def isPalindrome(self, x: int) -> bool:
+		if x < 0:
+			return False
+
+		x = str(x)
+
+		i = 0
+		j = len(x) - 1
+
+		while i <= j:
+			if x[i] == x[j]:
+				i += 1
+				j -= 1
+			else:
+				return False
+
+		return True
+
+
+class Solution:
+	def isPalindrome(self, x: int) -> bool:
+		x = str(x)
+		return x == x[::-1]
