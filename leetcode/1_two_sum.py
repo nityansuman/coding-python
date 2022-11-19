a945c9cae761f28273b2e928e26cef71a6073624
Kumar Nityan Suman <nityan.suman@gmail.com>
2022-11-19 17:43:08 +0000

    Adding solution

---
+++ leetcode/1_two_sum.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/two-sum/description/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict()
+
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [i, seen[target - nums[i]]]
+            else:
+                seen[nums[i]] = i
