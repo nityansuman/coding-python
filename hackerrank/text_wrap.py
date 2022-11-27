42dbfd92ac28c74ba0f83b26fea8b51c3dc0f5ce
Kumar Nityan Suman <nityan.suman@gmail.com>
2022-11-27 19:25:15 +0000

    Adding solution

---
+++ hackerrank/text_wrap.py
@@ -0,0 +1,13 @@
+def wrap(string: str, max_width: int) -> str:
+	tmp = ""
+	for i in range(len(string)):
+		if i % max_width == 0 and i != 0:
+			tmp += "\n"
+			tmp += string[i]
+		else:
+			tmp += string[i]
+	return tmp
+
+
+if __name__ == "__main__":
+	print(wrap("ABCDEFGHIJKLIMNOQRSTUVWXYZ", 4))
