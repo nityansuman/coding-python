15a98b3ad56ba20a2d848f00fa9bbe6f592bc1d7
Kumar Nityan Suman <nityan.suman@gmail.com>
2022-11-27 22:21:56 +0000

    Adding solution

---
+++ leetcode/234_palindome_linked_list.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/palindrome-linked-list/
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+	def isPalindrome(self, head: Optional[ListNode]) -> bool:
+		if head.next is None:
+			return True
+
+		num = ""
+		while head:
+			num += str(head.val)
+			head = head.next
+
+		return num == num[::-1]
